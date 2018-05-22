7e23650fa7769635b722d524ca37928ad9d5ff4e
ayesha2974 <ayesha.ismail3@gmail.com>
2018-05-22 15:02:18 +0300

    navinstructor-may22-ash

--- NavInstructor.js
+++ NavInstructor.js
@@ -10,11 +10,28 @@ export default class NavInstructor extends React.Component {
         return (
             <View style={styles.container}>
                 <View style={styles.navbar}>
+                    <View style={{ flexDirection: 'row' }}>
+                        <View>
+                            <Image style={styles.image} source={require('./panda.jpeg')} />
+                        </View>
+                        <View>
+                            <Text>Instructor Name</Text>
+                            <Text>60087635</Text>
+                            <TouchableOpacity>
+                                <Text>My Profile</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
 
+                    <View>
+                        <Text>Current Course</Text>
+                    </View>
+
+                    <View>
+                        <Text>Previous Course</Text>
+                    </View>
                 </View>
             </View>
-
-
         );
     }
 }
@@ -22,9 +39,22 @@ export default class NavInstructor extends React.Component {
 
 const styles = StyleSheet.create({
     container: {
+        width: '100%',
+        height: '100%',
         paddingTop: Constants.statusBarHeight,
+        backgroundColor: 'white'
     },
     navbar: {
-    }
+        backgroundColor: '#76323f',
+        width: 300,
+        height: '100%',
+        flexDirection: 'column'
+    },
+    image: {
+        height: 100,
+        width: 100,
+        borderRadius: 50,
+        margin: 10
+    },
 
 });
