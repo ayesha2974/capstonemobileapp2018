dd09d559941627dde971155a17aabd9782ab4949
meieldabi <60077702@cna-qatar.edu.qa>
2018-05-22 15:51:44 +0300

    mei_muna-FacultyList 22May

--- FacultyList.js
+++ FacultyList.js
@@ -3,12 +3,9 @@ import React from 'react';
 import Logo from './logo.png'
 import { Container, Header, Content, Form, Item, Input, Label, Button, Right } from 'native-base';
 import ModalDropdown from 'react-native-modal-dropdown';
-// import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
+import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
 
-
-
-export default class FaculyList extends React.Component {
-
+export default class FacultyList extends React.Component {
 
     render() {
         return (
@@ -20,26 +17,18 @@ export default class FaculyList extends React.Component {
 
                     </View> */}
                     <View style={styles.inline}>
-                        <Text style={{ marginTop: 10, fontSize: 20 }}>College ID  </Text>
-                        <TextInput underlineColorAndroid='transparent' style={{ borderColor: 'gray', width: 120, backgroundColor: 'white', height: 40, justifyContent: 'center' }} placeholder='        College ID' />
-                        <Text style={{ marginTop: 10, fontSize: 20, marginLeft: 15, height: 40 }}>Department </Text>
-
-                        {/* <Picker style={{width: 237}}>
-                    <Picker.Item label="Information Technology " value="jsaaa" />
-                                    <Picker.Item label="Engineering" value="java" />
-                                    <Picker.Item label="Health Science" value="js" />
-                          
-                    </Picker> */}
+                        <Text style={styles.titles}> College ID  </Text>
+                        <TextInput underlineColorAndroid='transparent' style={styles.inputfeilds} placeholder='College ID' />
+                        <Text style={styles.titles}>Department </Text>
                         <ModalDropdown
-                            style={{ borderColor: 'black', backgroundColor: 'white', height: 40, width: 300, marginRight: 30, paddingLeft: 10 }}
+                            style={styles.dropdowns}
                             options={['Information Technology', 'Health Sicence']}
-                            textStyle={{ fontSize: 20 }}
+                            textStyle={{ fontSize: 18 }}
                             dropdownTextStyle={{ fontSize: 20 }}
                         />
-                        <Text style={{ marginTop: 10, fontSize: 20, marginLeft: 15, height: 40 }}>Role  </Text>
-
+                        <Text style={styles.titles}>Role  </Text>
                         <ModalDropdown
-                            style={{ borderColor: 'black', backgroundColor: 'white', height: 40, width: 300, marginRight: 30, paddingLeft: 10 }}
+                            style={styles.dropdowns}
                             options={['Information Technology', 'Health Sicence']}
                             textStyle={{ fontSize: 20 }}
                             dropdownTextStyle={{ fontSize: 20 }}
@@ -47,43 +36,42 @@ export default class FaculyList extends React.Component {
                         <TouchableOpacity style={styles.button}>
                             <Text style={styles.buttonsText}>Search </Text>
                         </TouchableOpacity>
-
-                        {/* <View >
-                            <Table borderStyle={{ borderWidth: 2, borderColor: '#c8e1ff' }}>
+                    </View>
+                    {/* <View >
+                            <Table borderStyle={{ width: 1000, borderColor: '#c8e1ff',height: 1000, }}>
                                 <Row value={['COLLEGE ID','NAME', 'CONTACT NO', 'PERSINAL EMAIL', 'COLLEGE EMAIL', 'DEPARTMENT', 'ROLE']} style={styles.head} Style={styles.text} />
                                 <Rows value={[6000554, 'NAKKJHG','SHH',4545,]} Style={styles.text} />
                                 <Rows value={[6000554, 'NAKKJHG','SHH',4545,]} Style={styles.text} />
                                 <Rows value={[6000554, 'NAKKJHG','SHH',4545,]} Style={styles.text} />
                             </Table>
-                        </View> */}
-                        {/* <View style={{ flex: 1, flexDirection: 'row' }}>
+                        </View> 
+                        <View style={{ flex: 1, flexDirection: 'row' }}>
                             <View  style={{ width: 50, height: 50 }}/>  
                             <View style={{ width: 50, height: 50 }} />
                             <View style={{ width: 50, height: 50 }} />
-                        </View> */}
-                        {/* <View >
-                            <Label >College ID </Label>
-                            <Input style={styles.inputfeilds} placeholder='Username.Password' />
-                        </View> */}
-                        {/* <View>
-                            <Label>Department </Label>
-                            <Item >
-                                <Picker style={{ height: 50, width: 100 }}>
-                                    <Picker.Item label="a" value="java" />
-                                    <Picker.Item label="b" value="js" />
-                                </Picker>
-                            </Item>
+                        </View>  */}
+                    <View>
+                        <View style={styles.tableInline}>
+                            <Text style={styles.tabletitles}>Name </Text>
+                            <Text style={styles.tabletitles}>Contaact NO </Text>
+                            <Text style={styles.tabletitles}>Personal Email </Text>
+                            <Text style={styles.tabletitles}>College Email </Text>
+                            <Text style={styles.tabletitles}>Department </Text>
+                            <Text style={styles.tabletitles}>Role</Text>
                         </View>
-                        <View>
-                            <Label>Role </Label>
-                            <Item >
-                                <Picker style={{ height: 50, width: 100 }} >
-                                    <Picker.Item label="a" value="java" />
-                                    <Picker.Item label="b" value="js" />
-                                </Picker>
-                            </Item>
-                        </View> */}
                     </View>
+
+                        <View style={styles.tablebody}>
+                        {/* <Text>hhh</Text> */}
+                            <Text style={styles.tabletitles}>Nalin </Text>
+                            <Text style={styles.tabletitles}>77666655 </Text>
+                            <Text style={styles.tabletitles}>nalin@nalin.com </Text>
+                            <Text style={styles.tabletitles}>6001111@cna-qatar </Text>
+                            <Text style={styles.tabletitles}>Information T </Text>
+                            <Text style={styles.tabletitles}>IT</Text>
+                            <Text style={styles.tabletitles}>edit</Text>
+                        </View>
+                 
                 </KeyboardAvoidingView>
             </View>
 
@@ -93,17 +81,14 @@ export default class FaculyList extends React.Component {
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: 'white',
+        backgroundColor: '#e9e9e9',
         width: "100%",
         height: "100%",
         alignItems: 'center',
         justifyContent: 'center',
-        flex: 1, padding: 16, 
-        paddingTop: 30, 
-       
-
+        padding: 16,
+        paddingTop: 30,
     },
-
     pic: {
         width: 180,
         height: 160,
@@ -113,73 +98,53 @@ const styles = StyleSheet.create({
 
 
     inputfeilds: {
-        flex: 1,
-        justifyContent: 'center',
-        flexDirection: 'row',
-        textAlign: 'left',
-        width: 200,
-        borderWidth: 1,
-        color: 'black',
-        // marginTop: 10,
-        height: 20,
-        // marginBottom: "5%",
-        // marginLeft: "1%",
-        backgroundColor: 'white',
-        // borderColor: 'blue',
-        // borderWidth: 1,
-        fontSize: 20,
-    },
-
-    textStyle: {
-        textAlign: 'center',
-        fontWeight: 'bold',
-        fontSize: 25,
-        color: '#76323f',
-        width: 400,
-        marginTop: 25,
-
+        backgroundColor: 'white', height: 40, width: 80
     },
 
     inline: {
-        // flexWrap: 'wrap',
         flexDirection: 'row',
-        justifyContent: 'space-between'
+        width: '30%',
+        marginRight: 535,
+        // marginBottom:'50%'
     },
-
-    lbl1: {
-        marginRight: 50,
-        marginLeft: 50,
+    tableInline: {
+        backgroundColor: "#76323f", height: 60, width: 700, flexDirection: 'row',
+        // width: '30%',
+        marginLeft: 100,
+        marginRight: 110,
     },
-
     button: {
-        alignItems: 'center',
         backgroundColor: "#76323f",
-        padding: 2,
-        marginTop: 2,
-        width: 100,
-        // fontSize: 30,
-        borderRadius: 20,
-    },
-    buttons: {
-        marginTop: 5,
-        backgroundColor: "#fffaf0",
         width: 80,
-        color: 'white',
-        justifyContent: 'center',
-        textAlign: 'center',
-        marginLeft: 60,
-        fontWeight: 'bold',
+        height: 35,
+        borderRadius: 20,
+        marginLeft: 5,
+        marginRight: 100,
     },
-
     buttonsText: {
-
         color: 'white',
-        fontSize: 25,
-
-
+        fontSize: 15,
+        textAlign: 'center',
+        paddingTop: 4,
+        paddingLeft: 1
+        // padding: 3,
     },
 
-  head: { height: 40, backgroundColor: '#f1f8ff' },
-  text: { margin: 6 }
+    head: { height: 40, backgroundColor: '#f1f8ff' },
+    text: { margin: 6, color: 'black' },
 
+    titles: {
+        marginTop: 10, fontSize: 18, fontWeight: 'bold', fontSize: 18, marginLeft: 7, height: 40
+    },
+    tabletitles: {
+        marginTop: 10, fontSize: 18, fontWeight: 'bold', fontSize: 18, marginLeft: 7, height: 40, color: 'white'
+    },
+    dropdowns: {
+        borderColor: 'black', backgroundColor: 'white', height: 40, width: 150, marginLeft: 5, paddingLeft: 10
+    }
+    ,
+    tablebody: {
+        backgroundColor: "green", height: 500, width: 700,  marginLeft: 100,
+        marginRight: 110,  marginTop: 10, flexDirection: 'row'
+    },
 });
