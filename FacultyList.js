e0a621ff974819949c7efece9b169c15bd5c0be4
meieldabi <60077702@cna-qatar.edu.qa>
2018-05-23 15:47:02 +0300

    Mei_Muna-FacultyList+AdminProfile 23May

--- FacultyList.js
+++ FacultyList.js
@@ -4,6 +4,7 @@ import Logo from './logo.png'
 import { Container, Header, Content, Form, Item, Input, Label, Button, Right } from 'native-base';
 import ModalDropdown from 'react-native-modal-dropdown';
 import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
+// import FontAwesome, { Icons } from 'react-native-fontawesome';
 
 export default class FacultyList extends React.Component {
 
@@ -12,24 +13,20 @@ export default class FacultyList extends React.Component {
 
             <View style={styles.container} >
                 <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
-                    {/* <View style={styles.box} >
-                        <Image source={require('./logo.png')} style={styles.pic} />
-
-                    </View> */}
                     <View style={styles.inline}>
                         <Text style={styles.titles}> College ID  </Text>
                         <TextInput underlineColorAndroid='transparent' style={styles.inputfeilds} placeholder='College ID' />
                         <Text style={styles.titles}>Department </Text>
                         <ModalDropdown
                             style={styles.dropdowns}
-                            options={['Information Technology', 'Health Sicence']}
+                            options={['Information Technology', 'Health Sicence', 'Business Studies', 'Engineering Technology']}
                             textStyle={{ fontSize: 18 }}
                             dropdownTextStyle={{ fontSize: 20 }}
                         />
                         <Text style={styles.titles}>Role  </Text>
                         <ModalDropdown
                             style={styles.dropdowns}
-                            options={['Information Technology', 'Health Sicence']}
+                            options={['Instructor', 'Admin']}
                             textStyle={{ fontSize: 20 }}
                             dropdownTextStyle={{ fontSize: 20 }}
                         />
@@ -37,23 +34,11 @@ export default class FacultyList extends React.Component {
                             <Text style={styles.buttonsText}>Search </Text>
                         </TouchableOpacity>
                     </View>
-                    {/* <View >
-                            <Table borderStyle={{ width: 1000, borderColor: '#c8e1ff',height: 1000, }}>
-                                <Row value={['COLLEGE ID','NAME', 'CONTACT NO', 'PERSINAL EMAIL', 'COLLEGE EMAIL', 'DEPARTMENT', 'ROLE']} style={styles.head} Style={styles.text} />
-                                <Rows value={[6000554, 'NAKKJHG','SHH',4545,]} Style={styles.text} />
-                                <Rows value={[6000554, 'NAKKJHG','SHH',4545,]} Style={styles.text} />
-                                <Rows value={[6000554, 'NAKKJHG','SHH',4545,]} Style={styles.text} />
-                            </Table>
-                        </View> 
-                        <View style={{ flex: 1, flexDirection: 'row' }}>
-                            <View  style={{ width: 50, height: 50 }}/>  
-                            <View style={{ width: 50, height: 50 }} />
-                            <View style={{ width: 50, height: 50 }} />
-                        </View>  */}
                     <View>
                         <View style={styles.tableInline}>
+                            <Text style={styles.tabletitles}>ID </Text>
                             <Text style={styles.tabletitles}>Name </Text>
-                            <Text style={styles.tabletitles}>Contaact NO </Text>
+                            <Text style={styles.tabletitles}>Contact NO </Text>
                             <Text style={styles.tabletitles}>Personal Email </Text>
                             <Text style={styles.tabletitles}>College Email </Text>
                             <Text style={styles.tabletitles}>Department </Text>
@@ -61,17 +46,39 @@ export default class FacultyList extends React.Component {
                         </View>
                     </View>
 
-                        <View style={styles.tablebody}>
-                        {/* <Text>hhh</Text> */}
-                            <Text style={styles.tabletitles}>Nalin </Text>
-                            <Text style={styles.tabletitles}>77666655 </Text>
-                            <Text style={styles.tabletitles}>nalin@nalin.com </Text>
-                            <Text style={styles.tabletitles}>6001111@cna-qatar </Text>
-                            <Text style={styles.tabletitles}>Information T </Text>
-                            <Text style={styles.tabletitles}>IT</Text>
-                            <Text style={styles.tabletitles}>edit</Text>
-                        </View>
-                 
+                    <View style={styles.tablebody}>
+                        <Text style={styles.tableContent}>60076767 </Text>
+                        <Text style={styles.tableContent}>Nalin </Text>
+                        <Text style={styles.tableContent}>77666655 </Text>
+                        <Text style={styles.tableContent}>nalin@nalin.com </Text>
+                        <Text style={styles.tableContent}>6001111@cna-qatar </Text>
+                        <Text style={styles.tableContent}>IT  </Text>
+                        <Text style={styles.tableContent}>Instructor</Text>
+                        <Text style={styles.tableContent}>edit</Text>
+                        {/* <Text style={styles.tableContent}><FontAwesome>{Icons.chevronLeft}</FontAwesome></Text> */}
+
+                    </View>
+                    <View style={styles.tablebody}>
+                        <Text style={styles.tableContent}>60076767 </Text>
+                        <Text style={styles.tableContent}>Nalin </Text>
+                        <Text style={styles.tableContent}>77666655 </Text>
+                        <Text style={styles.tableContent}>nalin@nalin.com </Text>
+                        <Text style={styles.tableContent}>6001111@cna-qatar </Text>
+                        <Text style={styles.tableContent}>IT  </Text>
+                        <Text style={styles.tableContent}>Instructor</Text>
+                        <Text style={styles.tableContent}>edit</Text>
+                    </View>
+                    <View style={styles.tablebody}>
+
+                        <Text style={styles.tableContent}>60076767 </Text>
+                        <Text style={styles.tableContent}>Nalin </Text>
+                        <Text style={styles.tableContent}>77666655 </Text>
+                        <Text style={styles.tableContent}>nalin@nalin.com </Text>
+                        <Text style={styles.tableContent}>6001111@cna-qatar </Text>
+                        <Text style={styles.tableContent}>IT  </Text>
+                        <Text style={styles.tableContent}>Instructor</Text>
+                        <Text style={styles.tableContent}>edit</Text>
+                    </View>
                 </KeyboardAvoidingView>
             </View>
 
@@ -85,9 +92,8 @@ const styles = StyleSheet.create({
         width: "100%",
         height: "100%",
         alignItems: 'center',
-        justifyContent: 'center',
-        padding: 16,
-        paddingTop: 30,
+        // padding: 16,
+        paddingTop: 50,
     },
     pic: {
         width: 180,
@@ -103,15 +109,18 @@ const styles = StyleSheet.create({
 
     inline: {
         flexDirection: 'row',
-        width: '30%',
-        marginRight: 535,
+        width: '15%',
+        marginRight: 640,
+        marginLeft: 20
         // marginBottom:'50%'
+
     },
     tableInline: {
-        backgroundColor: "#76323f", height: 60, width: 700, flexDirection: 'row',
+        backgroundColor: "#76323f", height: 60, width: 740, flexDirection: 'row',
         // width: '30%',
         marginLeft: 100,
         marginRight: 110,
+
     },
     button: {
         backgroundColor: "#76323f",
@@ -134,17 +143,20 @@ const styles = StyleSheet.create({
     text: { margin: 6, color: 'black' },
 
     titles: {
-        marginTop: 10, fontSize: 18, fontWeight: 'bold', fontSize: 18, marginLeft: 7, height: 40
+        marginTop: 10, fontSize: 18, fontWeight: 'bold', marginLeft: 7, height: 40
     },
     tabletitles: {
-        marginTop: 10, fontSize: 18, fontWeight: 'bold', fontSize: 18, marginLeft: 7, height: 40, color: 'white'
+        marginTop: 10, fontSize: 16, fontWeight: 'bold', marginLeft: 28, height: 40, color: 'white'
+    },
+    tableContent: {
+        marginTop: 10, fontSize: 15, marginLeft: 25, height: 40, color: 'black'
     },
     dropdowns: {
         borderColor: 'black', backgroundColor: 'white', height: 40, width: 150, marginLeft: 5, paddingLeft: 10
     }
     ,
     tablebody: {
-        backgroundColor: "green", height: 500, width: 700,  marginLeft: 100,
-        marginRight: 110,  marginTop: 10, flexDirection: 'row'
+        height: 50, width: 300, marginLeft: 60,
+        marginRight: 530, marginTop: 10, flexDirection: 'row'
     },
 });
