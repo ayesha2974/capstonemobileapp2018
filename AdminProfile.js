cdf3d06eb26f719cb6ee4fb0e7ee7e72ef32f767
meieldabi <60077702@cna-qatar.edu.qa>
2018-05-24 19:04:34 +0300

    Mei_Muna AdminProfile 24May

--- AdminProfile.js
+++ AdminProfile.js
@@ -1,89 +1,62 @@
-import { AppRegistry, Text, TextInput, View, Alert, StyleSheet, TouchableOpacity, Image, KeyboardAvoidingView, Linking, ScrollView, Picker } from 'react-native';
+
 import React from 'react';
-import Logo from './logo.png'
-import { createBottomTabNavigator } from 'react-navigation'
+import { StyleSheet, Text, View,TextInput } from 'react-native';
+import Bar from 'react-native-bar-collapsible';
 
-class AdminProfile extends React.Component {
 
-    static navigationOptions = {
-        tabBarlabel: 'second',
-    }
+export default class Test2 extends React.Component {
     render() {
         return (
-            <View style={styles.container}>
-                <Text style={{ fontSize: 25 }}>Welcome to Home!</Text>
+            <View>
+                <Bar 
+                    title='Profile'
+                    collapsible={true}
+                    showOnStart={false}
+                    iconCollapsed='chevron-right'
+                    iconOpened='chevron-down'>
+                    <View style={styles.container}>
+                    <View style={styles.inline}>
+                        <Text style={styles.titles}> Name  </Text>
+                        <TextInput underlineColorAndroid='transparent' style={styles.inputfeilds} placeholder='Name' />
+               </View>
+               </View>
+                </Bar>
+                <Bar
+                    title='My title'
+                    collapsible={true}
+                    showOnStart={false}
+                    iconCollapsed='chevron-right'
+                    iconOpened='chevron-down'
+                >
+                    <Text> Shafeennnnnnnnnnn  </Text>
+                </Bar>
+
+
             </View>
-        )
+        );
     }
 }
-
-class Tab2 extends React.Component {
-
-    render() {
-        return (
-            <View style={{ marginTop: 25 }}>
-                <Text style={{ fontSize: 25, color: 'red' }} >Welcome to the Second Tab2!</Text>
-            </View>
-        )
-    }
-}
-
-class Tab3 extends React.Component {
-
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text style={{ fontSize: 25 }} >Welcome to  the Tab3!</Text>
-            </View>
-        )
-    }
-}
-class Tab4 extends React.Component {
-
-    render() {
-        return (
-            <View style={{ marginTop: 25 }}>
-                <Text style={{ fontSize: 25 }} >Welcome to the Tab4!</Text>
-            </View>
-        )
-    }
-}
-export default createBottomTabNavigator(
-
-    {
-        Home: { screen: AdminProfile },
-        Second: { screen: Tab2 },
-        Third: { screen: Tab3 },
-        Fourth: { screen: Tab4 },
-    },
-    {
-        tabBarPosition: "Top",
-        tabBarOptions: {
-            activeTintColor: 'grey',
-            inactiveTintColor: 'red',
-            backgroundColor: '#76323f',
-            showIcon: true,
-        },
-
-        // headerTintColor: 'black',
-
-
-        // style: {
-        // 	backgroundColor: 'orange',
-        // 	borderTopWidth: 1,
-        // 	borderTopColor: 'white'
-        // },
-    }
-);
-
 const styles = StyleSheet.create({
     container: {
         backgroundColor: '#e9e9e9',
-        // width: "100%",
-        // height: "100%",
-        // alignItems: 'center',
-        // // padding: 16,
-        // paddingTop: 50,
+        width: "100%",
+        height: "100%",
+        alignItems: 'center',
+        // padding: 16,
+        paddingTop: 50,
     },
+    inline: {
+        flexDirection: 'row',
+        width: '15%',
+        marginRight: 640,
+        marginLeft: 20
+        // marginBottom:'50%'
 
-});
+    },
+    titles: {
+        marginTop: 10, fontSize: 18, fontWeight: 'bold', marginLeft: 7, height: 40
+    },
+    inputfeilds: {
+        backgroundColor: 'white', height: 40, width: 80
+    },
+})
--- App.js
+++ App.js
@@ -3,14 +3,9 @@ import { StyleSheet, Text, View } from 'react-native';
 import Login from './login'
 import Reset from './Reset'
 import EnterPass from './EnterPass'
-<<<<<<< HEAD
 import NavInstructor from './NavInstructor'
 import NavStudent from './NavStudent'
 import Chat from './Chat'
-=======
- import NavInstructor from './NavInstructor'
-// import Chat from './Chat'
->>>>>>> a190c02d427ded015471475fdedab95be0d4f7f2
 import AdminRegister from './AdminRegister'
 // import FacultyList from './FacultyList'
 import InstructorGradePage from './InstructorGradePage'
@@ -25,27 +20,19 @@ export default class App extends React.Component {
         {/* <Login />  */}
         {/* <Reset/>  */}
         {/* <EnterPass /> */}
-<<<<<<< HEAD
         {/* <NavInstructor /> */}
         {/* <NavStudent /> */}
-        <Chat />
-=======
-        <NavInstructor />
         {/* <Chat /> */}
->>>>>>> a190c02d427ded015471475fdedab95be0d4f7f2
         {/* <FacultyList/> */}
         {/* <AdminRegister /> */}
         {/* <InstructorExam/> */}
         {/* {<InstructorGradePage/>} */}
-<<<<<<< HEAD
 
         {/* <StudentExam /> */}
-=======
-        {/* <StudentExam/> */}
-        {/* {<NavStudent/>} */}
-
-        
->>>>>>> a190c02d427ded015471475fdedab95be0d4f7f2
+         {/* <Test /> */}
+         {/* <Test2 />  */}
+         {/* <Test3 /> */}
+         <AdminProfile/> 
       </View>
     );
   }
